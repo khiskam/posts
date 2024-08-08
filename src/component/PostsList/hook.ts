0e88287a5d0c0c4b4a1960d4f0fda9ee12713a53
Kamilla Khisamieva <khisamieva0409@gmail.com>
2024-08-08 12:06:10 +0300

    change replace on push

--- src/component/PostsList/hook.ts
+++ src/component/PostsList/hook.ts
@@ -3,14 +3,14 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 export const usePage = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { replace } = useRouter();
+  const { push } = useRouter();
 
   const setPage = (page: number) => {
     const params = new URLSearchParams(searchParams);
 
     params.set("page", page.toString());
 
-    replace(`${pathname}?${params.toString()}`);
+    push(`${pathname}?${params.toString()}`);
   };
 
   const getPage = () => {
